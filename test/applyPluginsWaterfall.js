648738384ff4714304b1820c0689686a897b370a
Dom Farolino <domfarolino@gmail.com>
2017-01-25 15:55:13 -0500

    Tabsize 4

--- test/applyPluginsWaterfall.js
+++ test/applyPluginsWaterfall.js
@@ -10,76 +10,76 @@ var should = require("should");
  * returns some value (implicitly undefined if none specified).
  */
 function makeTestPlugin(expectedArgs, returnVal) {
-  return function() {
-    var args = Array.prototype.slice.call(arguments);
-    args.should.be.eql(expectedArgs);
-    return returnVal;
-  }
+	return function() {
+		var args = Array.prototype.slice.call(arguments);
+		args.should.be.eql(expectedArgs);
+		return returnVal;
+	}
 }
 
 describe("applyPluginsWaterfall", function() {
 
-  it("should call all handlers", function() {
-    var runningSum = 0;
-    var tapable = new Tapable();
-    
-    tapable.plugin('plugin', function() {
-      runningSum++;
-    });
-    
-    tapable.plugin('plugin', function() {
-      runningSum++;
-    });
-    
-    tapable.plugin('plugin', function() {
-      runningSum++;
-    });
+	it("should call all handlers", function() {
+		var runningSum = 0;
+		var tapable = new Tapable();
 
-    tapable.applyPluginsWaterfall('plugin');
-    runningSum.should.be.eql(3);
-  });
+		tapable.plugin('plugin', function() {
+			runningSum++;
+		});
 
-  it("should call first handler with init value", function() {
-    var tapable = new Tapable();  
-    var pluginHandler = makeTestPlugin(['initValue']);
-    
-    tapable.plugin('plugin', pluginHandler);
-    tapable.applyPluginsWaterfall('plugin', 'initValue');
-  });
+		tapable.plugin('plugin', function() {
+			runningSum++;
+		});
 
-  it("should call subsequent handlers with previous handler return value", function() {
-    var tapable = new Tapable();
-    
-    var pluginHandler1 = makeTestPlugin(['initValue'], 'handler1Return');
-    var pluginHandler2 = makeTestPlugin(['handler1Return'], 'handler2Return');
-    var pluginHandler3 = makeTestPlugin(['handler2Return'], 'handler3Return');
-    var pluginHandler4 = makeTestPlugin(['handler3Return']);
+		tapable.plugin('plugin', function() {
+			runningSum++;
+		});
 
-    tapable.plugin('plugin', pluginHandler1);
-    tapable.plugin('plugin', pluginHandler2);
-    tapable.plugin('plugin', pluginHandler3);
-    tapable.plugin('plugin', pluginHandler4);
+		tapable.applyPluginsWaterfall('plugin');
+		runningSum.should.be.eql(3);
+	});
 
-    tapable.applyPluginsWaterfall('plugin', 'initValue');
-  });
+	it("should call first handler with init value", function() {
+		var tapable = new Tapable();
+		var pluginHandler = makeTestPlugin(['initValue']);
 
-  it("should call subsequent handlers with original arguments", function() {
-    var tapable = new Tapable();
-    var allArgs = ['plugin', 'initValue', 'sharedArg1', 'sharedArg2', 'sharedArg3'];
-    var sharedArgs = allArgs.slice(2); // arguments that each plugin handler will get
-    
-    var pluginHandler1 = makeTestPlugin(allArgs.slice(1), 'handler1Return');
-    var pluginHandler2 = makeTestPlugin(['handler1Return'].concat(sharedArgs), 'handler2Return');
-    var pluginHandler3 = makeTestPlugin(['handler2Return'].concat(sharedArgs), 'handler3Return');
-    var pluginHandler4 = makeTestPlugin(['handler3Return'].concat(sharedArgs));
+		tapable.plugin('plugin', pluginHandler);
+		tapable.applyPluginsWaterfall('plugin', 'initValue');
+	});
 
-    tapable.plugin('plugin', pluginHandler1);
-    tapable.plugin('plugin', pluginHandler2);
-    tapable.plugin('plugin', pluginHandler3);
-    tapable.plugin('plugin', pluginHandler4);
+	it("should call subsequent handlers with previous handler return value", function() {
+		var tapable = new Tapable();
 
-    // Calling apply to simulate ...spreadOperator
-    tapable.applyPluginsWaterfall.apply(tapable, allArgs);
-  });
+		var pluginHandler1 = makeTestPlugin(['initValue'], 'handler1Return');
+		var pluginHandler2 = makeTestPlugin(['handler1Return'], 'handler2Return');
+		var pluginHandler3 = makeTestPlugin(['handler2Return'], 'handler3Return');
+		var pluginHandler4 = makeTestPlugin(['handler3Return']);
+
+		tapable.plugin('plugin', pluginHandler1);
+		tapable.plugin('plugin', pluginHandler2);
+		tapable.plugin('plugin', pluginHandler3);
+		tapable.plugin('plugin', pluginHandler4);
+
+		tapable.applyPluginsWaterfall('plugin', 'initValue');
+	});
+
+	it("should call subsequent handlers with original arguments", function() {
+		var tapable = new Tapable();
+		var allArgs = ['plugin', 'initValue', 'sharedArg1', 'sharedArg2', 'sharedArg3'];
+		var sharedArgs = allArgs.slice(2); // arguments that each plugin handler will get
+
+		var pluginHandler1 = makeTestPlugin(allArgs.slice(1), 'handler1Return');
+		var pluginHandler2 = makeTestPlugin(['handler1Return'].concat(sharedArgs), 'handler2Return');
+		var pluginHandler3 = makeTestPlugin(['handler2Return'].concat(sharedArgs), 'handler3Return');
+		var pluginHandler4 = makeTestPlugin(['handler3Return'].concat(sharedArgs));
+
+		tapable.plugin('plugin', pluginHandler1);
+		tapable.plugin('plugin', pluginHandler2);
+		tapable.plugin('plugin', pluginHandler3);
+		tapable.plugin('plugin', pluginHandler4);
+
+		// Calling apply to simulate ...spreadOperator
+		tapable.applyPluginsWaterfall.apply(tapable, allArgs);
+	});
 
 });
