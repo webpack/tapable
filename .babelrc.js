c094c70d3f7c172574f1e7b2851282f055ade5db
alexander-akait <sheo13666q@gmail.com>
2025-12-18 20:50:55 +0300

    ci: exclude Node.js6 because it is untestable

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,4 @@
-// TDOO remove me when Node.js@6 will be dropped
-/* eslint-disable */
-if (!Object.entries) {
-	Object.entries = function (obj) {
-		const ownProps = Object.keys(obj);
-		let i = ownProps.length;
-		const resArray = new Array({ length: i }); // Preallocate the array
-
-		while (i--) {
-			resArray[i] = [ownProps[i], obj[ownProps[i]]];
-		}
-		return resArray;
-	};
-}
-/* eslint-enable */
+"use strict";
 
 module.exports = {
 	presets: [
